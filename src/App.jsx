c0bb7cbfcb2b8ce96acbc8902b5293f049c6b249
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-09-12 14:08:51 +0000

    add advice fetching functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
-  return <></>;
+  const [adviceData, setAdviceData] = useState({});
+
+  const API_URL = "https://api.adviceslip.com/advice";
+
+  async function fetchAdvice() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    setAdviceData(data.slip);
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <p>{adviceData.id}</p>
+      </div>
+
+      <div>
+        <p> {adviceData.advice || "Loading advice..."} </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
